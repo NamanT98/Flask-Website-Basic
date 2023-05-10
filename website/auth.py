2280c31ae4be3f311c26bb04d0610036f4d2a306
namant <namant@iitbhilai.ac.in>
2023-05-10 21:00:54 +0530

    changed user name

--- website/auth.py
+++ website/auth.py
@@ -4,7 +4,7 @@ auth=Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
-    return render_template('login.html',text='testing',user='Naman')
+    return render_template('login.html',text='testing',user='Naman Trisoliya')
 
 @auth.route('/logout')
 def logout():
